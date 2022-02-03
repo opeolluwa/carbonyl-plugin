e7162b12e04c8506693dcae42567b1dbbbf7f694
Opeoluwa Adeoye <adefemiadeoye@yahoo.com>
2022-02-03 17:04:26 +0100

    CHORE :: added logic for minifying the links

--- controllers/minify.js
+++ controllers/minify.js
@@ -1,7 +1,23 @@
-function minify(req, res) {
+const { nanoid } = require('nanoid/async'); //nanoid
+const { MinifiedUrl } = require('../models');
+
+async function minify(req, res) {
+    //get rl from payload
     const { url } = req.body;
 
-    res.send(url)
+    //assign unique id to the url and store it to database
+    try {
+        
+        const uuid = await nanoid(8)
+        const longUrl = url;
+        const shortenedUrl = 'https://minifier.mdbgo.io/' + uuid
+        await MinifiedUrl.create({ longUrl, shortenedUrl })
+
+        //send feedback to client
+        res.send({ error: false, uuid, url })
+    } catch (error) {
+        res.send({ error: true, message: "An internal error occurred, please try again after some time" })
+    }
 }
 
 
