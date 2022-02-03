b06de1488a9061ed1fd70862ebb1c815bbb3d748
Opeoluwa Adeoye <adefemiadeoye@yahoo.com>
2022-02-03 22:34:22 +0100

    UPDATE

--- controllers/match.js
+++ controllers/match.js
@@ -1,9 +1,12 @@
-function match(req, res) {
-    const { url } = req.body;
+const { MinifiedUrl } = require('../models');
 
-    res.send(url)
+async function match(req, res) {
+    //get minified url id from payload
+    //<baseurl>/url-id //https://m.t/khfklam
+    const shortenedUrlId = req.params.id;
+    const shortenedUrl = await MinifiedUrl.findOne({ where: { shortenedUrlId } })
+    const actualLink = shortenedUrl.longUrl
+    res.redirect(actualLink)
 }
 
-
-
-module.exports = { match }
+module.exports = { match } 
