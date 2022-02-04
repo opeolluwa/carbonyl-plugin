90bd1044c7765acaef773cafac1bce44d6b3aaa5
Opeoluwa Adeoye <adefemiadeoye@yahoo.com>
2022-02-04 07:57:14 +0100

    CHORE :: set urlid to unique field in data table

--- controllers/custom-id.js
+++ controllers/custom-id.js
@@ -8,12 +8,23 @@ async function customId(req, res) {
     const link = minifiedUrl.split("/")
     const shortenedUrlId = link[link.length - 1]
 
-    try {
-        const shortenedUrl = await MinifiedUrl.findOne({ where: { shortenedUrlId } });
 
-        // if no match send error
+    //reject keyword longer than 25 characters in length
+    if (keyword.length > 25) {
+        return res.send({ error: true, message: "keyword too long, consider using words not up to 25 characters in length" })
+    }
+
+    try {
+        //fetch data associated with the provided id send error if no match
+        const shortenedUrl = await MinifiedUrl.findOne({ where: { shortenedUrlId } });
         if (!shortenedUrl) {
-            return res.status(403)
+            return res.status(400).send({ error: true, message: "url not found" })
+        }
+
+        //if keyword already exist, send error
+        const isUniqueId = await MinifiedUrl.findOne({ where: { shortenedUrlId: keyword } });
+        if (isUniqueId) {
+            return res.status(403).send({ error: true, message: "keyword exist, please consider using another keyword" })
         }
 
         //else update the url keyword
