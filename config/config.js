71e2906c6a2e4a515b6f8c10e8dd938226ecf3b9
Opeoluwa Adeoye <adefemiadeoye@yahoo.com>
2022-02-05 07:34:10 +0100

    update

--- config/config.js
+++ config/config.js
@@ -15,10 +15,10 @@ module.exports = {
     "dialect": "mysql"
   },
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PASS,
+    "database": process.env.DB_SCHEMA,
+    "host": process.env.BD_HOST,
     "dialect": "mysql"
   }
 }
