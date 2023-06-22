f7a5d7e3c1db7d068bd4ace328c2e8ff34a04f2c
Adeoye Adefemi <adefemiadeoye@yahoo.com>
2023-06-22 01:52:52 +0100

    docs

--- routes/custom-id.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const cors = require('cors');
-const { customId } = require('../controllers/custom-id');
-const router = express.Router()
-router.use(cors())
-//POST <base-url>/api/custom-id
-router.post("/custom-id", customId)
-
-module.exports = router;
